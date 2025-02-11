a1938e3d9aa6c3ab429d9a741e51616f0c058058
Tom <73725515+TomC13K@users.noreply.github.com>
2025-02-11 13:31:08 +0000

    adding local ollama LLM to processing the prompts

--- ice_breaker.py
+++ ice_breaker.py
@@ -16,14 +16,15 @@ if __name__ == '__main__':
 
     # {} is param for the prompt
     summary_template = """
-        given the information {information} about a person from I want you to create:
+        given the information {information} about a person, I want you to create:
         1. a short summary
         2. two interesting facts about them
     """
     summary_prompt_template = PromptTemplate(input_variables=["information"], template=summary_template)
 
     #llm = ChatOpenAI(temperature=0, tiktoken_model_name="gpt-4o-mini")
-    llm = ChatOllama(model="deepseek-r1:32b")
+    #llm = ChatOllama(model="deepseek-r1:32b")
+    llm = ChatOllama(model="llama3.1")
 
     chain = summary_prompt_template | llm
 
